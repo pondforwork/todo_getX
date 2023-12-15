f7be23cb85f3531b8832aa18d062a18279081574
pondzaa114 <pharadonsiriforwork@gmail.com>
2023-12-15 11:48:37 +0700

    AddToDo with GetX

--- lib/view/viewtodo.dart
+++ lib/view/viewtodo.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:project_todo_getx/controller/todocontroller.dart';
+import 'package:project_todo_getx/models/todo.dart';
 
 class ViewToDo extends StatelessWidget {
   ViewToDo({super.key});
@@ -44,11 +45,17 @@ class ViewToDo extends StatelessWidget {
                     );
                   });
             },
-          )),FloatingActionButton(onPressed: (){
-            print("Press");
-          })
+          )),
         ],
       )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          ToDo newtd =
+              ToDo("2222", "12345", false, Colors.black, DateTime.now());
+          todocontroller.addToDo(newtd);
+        },
+        child: Icon(Icons.add),
+      ),
     );
   }
 }

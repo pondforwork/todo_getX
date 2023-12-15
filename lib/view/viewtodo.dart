6e054de84a1780b7c131fc44db0527deb155822c
pondzaa114 <pharadonsiriforwork@gmail.com>
2023-12-15 12:13:13 +0700

    Show Old Tile

--- lib/view/viewtodo.dart
+++ lib/view/viewtodo.dart
@@ -21,27 +21,72 @@ class ViewToDo extends StatelessWidget {
               return ListView.builder(
                   itemCount: controller.todo.length,
                   itemBuilder: (BuildContext context, int index) {
-                    return Card(
-                      margin: EdgeInsets.all(12),
-                      child: Padding(
-                        padding: EdgeInsets.all(16),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.end,
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text('${controller.todo[index].topic}'),
-                                  ],
-                                )
-                              ],
-                            )
-                          ],
+                    return GestureDetector(
+                      onTap: () {
+                        print("Tap");
+                        print(controller.todo[index].id);
+                      },
+                      child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(
+                            10.0), // Set the border radius
+                        side: const BorderSide(
+                            color: Color.fromARGB(255, 255, 0, 0), // i need to apply my col here
+                            width: 1), // Set the border color and width
+                      ),
+                      child: GestureDetector(
+                        onLongPress: () {
+                          // print(todo.id);
+                          // _showDeleteDialog(todo.id);
+                        },
+                        child: ListTile(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          // tileColor: controller.todo[index].color,
+                          tileColor: Colors.red,
+
+                          leading: Checkbox(
+                            activeColor: Colors
+                                .white, // Set the color of the check mark when checked
+                            checkColor: const Color.fromARGB(255, 8, 7,
+                                7), // Set the color of the check mark
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(
+                                  8.0), // Set the border radius of the checkbox box
+                              side: const BorderSide(
+                                  color: Color.fromARGB(255, 9, 80,
+                                      138)), // Set the border color and width
+                            ),
+                            value: controller.todo[index].isfinish,
+                            onChanged: (bool? value) {
+                                
+                              // setState(() {
+                              //   todo.isfinish = !todo.isfinish;
+                              // });
+                              // setState(() {
+                              //   addOrUpdateData(todo.id, todo.topic,
+                              //       todo.isfinish, todo.color, todo.order);
+                              //   // _data = dbHelper.getData();
+                              // });
+                            },
+                          ),
+                          title: Container(
+                            width: 300,
+                            height: 50,
+                            padding: const EdgeInsets.all(8.0),
+                            child: Center(
+                              child: Text(
+                                controller.todo[index].topic,
+                                style: const TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ),
                         ),
                       ),
+                    ),
                     );
                   });
             },
@@ -50,14 +95,14 @@ class ViewToDo extends StatelessWidget {
       )),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          ToDo newtd =
-              ToDo("222112", "12345", false, Colors.black, DateTime.now());
+          ToDo newtd = ToDo("222112fgdsdfgs", "12345adfsdfgs", false,
+              Colors.black, DateTime.now());
 
           todocontroller.addData(
               newtd.id, newtd.topic, newtd.isfinish, newtd.color, newtd.order);
           todocontroller.addToDo(newtd);
         },
-        child: Icon(Icons.add),
+        child: const Icon(Icons.add),
       ),
     );
   }

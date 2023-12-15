ba8bd21403ac0089ba7683414d034677cd2d6043
pondzaa114 <pharadonsiriforwork@gmail.com>
2023-12-15 22:46:17 +0700

    Delay 3S

--- lib/view/viewtodo2.dart
+++ lib/view/viewtodo2.dart
@@ -3,7 +3,8 @@ import 'package:get/get.dart';
 import 'package:project_todo_getx/controller/todocontroller.dart';
 import 'package:project_todo_getx/models/todo.dart';
 
-class ViewToDo extends StatefulWidget {  final todocontroller = Get.put(ToDoController());
+class ViewToDo extends StatefulWidget {
+  final todocontroller = Get.put(ToDoController());
 
   ViewToDo({super.key});
 
@@ -12,7 +13,7 @@ class ViewToDo extends StatefulWidget {  final todocontroller = Get.put(ToDoCont
 }
 
 class _ViewToDoState extends State<ViewToDo> {
-    final todocontroller = Get.put(ToDoController());
+  final todocontroller = Get.put(ToDoController());
 
   @override
   Widget build(BuildContext context) {
@@ -34,68 +35,81 @@ class _ViewToDoState extends State<ViewToDo> {
                         print(controller.todo[index].id);
                       },
                       child: Card(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(
-                            10.0), // Set the border radius
-                        side: const BorderSide(
-                            color: Color.fromARGB(255, 255, 0, 0), // i need to apply my col here
-                            width: 1), // Set the border color and width
-                      ),
-                      child: GestureDetector(
-                        onLongPress: () {
-                          // print(todo.id);
-                          // _showDeleteDialog(todo.id);
-                        },
-                        child: ListTile(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                          // tileColor: controller.todo[index].color,
-                          tileColor: Colors.red,
-
-                          leading: Checkbox(
-                            activeColor: Colors
-                                .white, // Set the color of the check mark when checked
-                            checkColor: const Color.fromARGB(255, 8, 7,
-                                7), // Set the color of the check mark
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(
+                              10.0), // Set the border radius
+                          side: const BorderSide(
+                              color: Color.fromARGB(255, 255, 0,
+                                  0), // i need to apply my col here
+                              width: 1), // Set the border color and width
+                        ),
+                        child: GestureDetector(
+                          onLongPress: () {
+                            // print(todo.id);
+                            // _showDeleteDialog(todo.id);
+                          },
+                          child: ListTile(
                             shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(
-                                  8.0), // Set the border radius of the checkbox box
-                              side: const BorderSide(
-                                  color: Color.fromARGB(255, 9, 80,
-                                      138)), // Set the border color and width
+                              borderRadius: BorderRadius.circular(10.0),
                             ),
-                            value: controller.todo[index].isfinish,
-                            onChanged: (bool? value) {
+                            // tileColor: controller.todo[index].color,
+                            tileColor: Colors.red,
+
+                            leading: Checkbox(
+                              activeColor: Colors
+                                  .white, // Set the color of the check mark when checked
+                              checkColor: const Color.fromARGB(255, 8, 7,
+                                  7), // Set the color of the check mark
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(
+                                    8.0), // Set the border radius of the checkbox box
+                                side: const BorderSide(
+                                    color: Color.fromARGB(255, 9, 80,
+                                        138)), // Set the border color and width
+                              ),
+                              value: controller.todo[index].isfinish,
+                              onChanged: (bool? value) {
                                 value = !value!;
                                 print(value);
-                              setState(() {
-                                controller.todo[index].isfinish = !controller.todo[index].isfinish;
+                                setState(() {
+                                  controller.todo[index].isfinish =
+                                      !controller.todo[index].isfinish;
+                                  Future.delayed(const Duration(seconds: 3), () {
+                                    controller.addData(
+                                        controller.todo[index].id,
+                                        controller.todo[index].topic,
+                                        controller.todo[index].isfinish,
+                                        controller.todo[index].color,
+                                        controller.todo[index].order);
+                                  });
+                                });
+
+                                // controller.fetchProducts();
                                 // todo.isfinish = !todo.isfinish;
-                              });
-                              // setState(() {
-                              //   addOrUpdateData(todo.id, todo.topic,
-                              //       todo.isfinish, todo.color, todo.order);
-                              //   // _data = dbHelper.getData();
-                              // });
-                            },
-                          ),
-                          title: Container(
-                            width: 300,
-                            height: 50,
-                            padding: const EdgeInsets.all(8.0),
-                            child: Center(
-                              child: Text(
-                                controller.todo[index].topic,
-                                style: const TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold),
+
+                                // setState(() {
+                                //   addOrUpdateData(todo.id, todo.topic,
+                                //       todo.isfinish, todo.color, todo.order);
+                                //   // _data = dbHelper.getData();
+                                // });
+                              },
+                            ),
+                            title: Container(
+                              width: 300,
+                              height: 50,
+                              padding: const EdgeInsets.all(8.0),
+                              child: Center(
+                                child: Text(
+                                  controller.todo[index].topic,
+                                  style: const TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold),
+                                ),
                               ),
                             ),
                           ),
                         ),
                       ),
-                    ),
                     );
                   });
             },
